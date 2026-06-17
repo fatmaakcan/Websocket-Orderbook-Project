1ae53616f3d3f44416bc382854448f5fabe59344
Fatma Akcan <fatma.akcan.dev@gmail.com>
2026-06-17 19:51:32 +0300

    feat: implement dynamic order submission and json serialization

--- client_trader.py
+++ client_trader.py
@@ -1,10 +1,23 @@
 import asyncio
 from websockets.asyncio.client import connect
+import json
 
 async def hello():
     async with connect("ws://localhost:9000") as websocket:
         print("Client: Sunucuya bağlanıldı, mesaj gönderiliyor...")
-        await websocket.send("Hello world!")
+        user=input("User: ")
+        side=input("Sell or buy: ")
+        price=float(input("Price: "))
+        quantity=float(input("Quantity: "))
+
+        information={
+            "user":user,
+            "side":side,
+            "price":price,
+            "quantity":quantity
+        }
+        json_str=json.dumps(information,ensure_ascii=False,indent=4)
+        await websocket.send(json_str)
         message = await websocket.recv()
         print(f"Client'a sunucudan gelen cevap: {message}")
     
--- server.py
+++ server.py
@@ -1,11 +1,20 @@
 import asyncio
 from websockets.asyncio.server import serve
+import json
+
+buy_orders = []
+sell_orders = []
+trades = []
 
 async def echo(websocket):
     print("sunucu: bir client bağlandı!")
-    async for message in websocket:
-        await websocket.send(message)
-
+    async for message in websocket:  
+        order=json.loads(message)
+        if order["side"]=="buy":
+            buy_orders.append(order)
+        elif order["side"]=="sell":
+            sell_orders.append(order)
+        await websocket.send("Emriniz alındı!")
 async def main():
     async with serve(echo, "localhost",9000) as server:
         print("sunucu ayağa kalktı, port 9000 dinleniyor...")
